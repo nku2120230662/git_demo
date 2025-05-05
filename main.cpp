d19e64f941c3a276d041dce64dec76f926b2ef70
DGH19 <duanguoha0@163.com>
2025-05-05 22:24:43 +0800

    add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "util.h"
 using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    int a = 5;
+    int b = 10;
+    int sum = add(a, b);
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
 }
